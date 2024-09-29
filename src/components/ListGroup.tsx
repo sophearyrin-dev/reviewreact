956575638636d155dd8f54fd74b8bf213e80f36a
sophearyrin1 <srin@miu.edu>
2024-09-28 22:37:41 -0500

    rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,28 @@
+import { Fragment } from "react/jsx-runtime";
+
 function ListGroup() {
+  const items = [
+    "An item one",
+    "A second item",
+    "A third item",
+    "A fourth item",
+    "And a fifth one",
+  ];
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <Fragment>
+      <ul className="list-group">
+        {/* <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li> */}
+        {items.map((item, index) => (
+          <li key={index} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
